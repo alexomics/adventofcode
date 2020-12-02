f826c7b7eba89763d844e6aaec662b8f71140b3c
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-02 09:55:22 +0000

    2020 day 2 update

--- 2020/day02.py
+++ 2020/day02.py
@@ -13,3 +13,11 @@ with open(sys.argv[1], "r") as fh:
             p2 += 1
 
 print(f"Part 1: {p1}\nPart 2: {p2}")
+
+"""Minimised:
+import sys, re
+
+parts = [re.findall("(\d+)-(\d+) (\w+): (\w+)", l.strip())[0] for l in open(sys.argv[1])]
+print(f"Part 1: {sum(int(x) <= p.count(c) <= int(y) for x, y, c, p in parts)}")
+print(f"Part 2: {sum((p[int(x) - 1] == c) + (p[int(y) - 1] == c) == 1 for x, y, c, p in parts)}")
+"""
