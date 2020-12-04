7ed9feeedb159d85e2f03963733d7151f9cea9f2
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-04 06:02:11 +0000

    2020 day 4

---
+++ 2020/day04.py
@@ -0,0 +1,42 @@
+import sys
+import re
+
+
+fields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+print("Part 1:", end=" ")
+print(
+    sum(
+        [
+            set(re.findall("(\w+):", l)).issuperset(fields)
+            for l in open(sys.argv[1]).read().split("\n\n")
+        ]
+    )
+)
+
+colour = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+lines = [re.findall("(\w+):(\S+)", l) for l in open(sys.argv[1]).read().split("\n\n")]
+
+x = 0
+for passport in lines:
+    if not set([f[0] for f in passport]).issuperset(fields):
+        continue
+
+    d = dict(passport)
+
+    x += all(
+        [
+            1920 <= int(d["byr"]) <= 2002,
+            2010 <= int(d["iyr"]) <= 2020,
+            2020 <= int(d["eyr"]) <= 2030,
+            (
+                (d["hgt"].endswith("cm") and 150 <= int(d["hgt"][:-2]) <= 193)
+                or (d["hgt"].endswith("in") and 59 <= int(d["hgt"][:-2]) <= 76)
+            ),
+            bool(re.fullmatch(r"#[\da-f]{6}", d["hcl"])),
+            d["ecl"] in colour,
+            bool(re.fullmatch("\d{9}", d["pid"])),
+        ]
+    )
+
+print(f"Part 2: {x}")
