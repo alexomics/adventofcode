8a54b1fb5dbe266a85ce635ea2b9f52058b08581
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-16 07:18:22 +0000

    2020 day 16
    Better parsing, a bit more readable

--- 2020/day16.py
+++ 2020/day16.py
@@ -1,30 +1,14 @@
 import sys
 
-lines = [l for l in open(sys.argv[1])]
-
-rules = []
-ticket = []
-nearby = []
-nb = False
-
-for i in range(len(lines)):
-    l = lines[i].strip()
-    if not l:
-        continue
-    p = l.split(": ")
-    if len(p) > 1:
-        r = p[0]
-        n = [list(map(int, x.split("-"))) for x in p[1].split(" or ")]
-        rules.append((r, n))
-    if l.startswith("your"):
-        ticket = list(map(int, lines[i + 1].split(",")))
-    if l.startswith("nearby"):
-        nb = True
-        continue
-    if nb:
-        nearby.append(list(map(int, l.split(","))))
-
-rules = dict(rules)
+rules, ticket, nearby = [l.splitlines() for l in open(sys.argv[1]).read().split("\n\n")]
+rules = {
+    l.split(": ")[0]: [
+        list(map(int, x.split("-"))) for x in l.split(": ")[1].split(" or ")
+    ]
+    for l in rules
+}
+ticket = list(map(int, ticket[1].split(",")))
+nearby = [list(map(int, l.split(","))) for l in nearby[1:]]
 
 c = 0
 for t in nearby:
@@ -32,35 +16,33 @@ for t in nearby:
         p = any(a <= i <= b for v in rules.values() for a, b in v)
         if not p:
             c += i
-print(c)
+print(f"Part 1: {c}")
 
-valid_nearby = []
-for t in nearby:
-    if all(any(a <= n <= b for v in rules.values() for a, b in v) for n in t):
-        valid_nearby.append(t)
+valid_nearby = [
+    t
+    for t in nearby
+    if all(any(a <= n <= b for v in rules.values() for a, b in v) for n in t)
+]
 
 fields = [set(rules.keys()) for _ in ticket]
 
 for t in valid_nearby:
-    for i in range(len(t)):
-        value = t[i]
+    for i, value in enumerate(t):
         for k, v in rules.items():
-            ok1 = any(a <= value <= b for a, b in v)
-            if not ok1:
+            if not any(a <= value <= b for a, b in v):
                 fields[i].remove(k)
 
-for k in range(len(fields)):
+while not all(len(f) == 1 for f in fields):
     for i in range(len(fields)):
-        if len(fields[i]) == 1:
+        f = fields[i]
+        if len(f) == 1:
             for j in range(len(fields)):
                 if j != i:
-                    fields[j] -= fields[i]
+                    fields[j] -= f
 
 c = 1
-
-for i in range(len(ticket)):
+for i, t in enumerate(ticket):
     for field in fields[i]:
         if field.startswith("departure"):
-            c *= ticket[i]
-
-print(c)
+            c *= t
+print(f"Part 2: {c}")
