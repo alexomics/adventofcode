2bf2d7077527ca56c98de86264a9047d76816014
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-17 08:09:19 +0000

    2020 day 17
    Generalised solution for n dimensions, should work for ≥2 dimensions.

--- 2020/day17.py
+++ 2020/day17.py
@@ -2,37 +2,39 @@ import sys
 from itertools import product
 
 
+class Grid(dict):
+    def bounds(self):
+        return zip(map(min, zip(*self.keys())), map(max, zip(*self.keys())))
+
+    def neighbour_deltas(self):
+        dims = len(list(self.keys())[0])
+        yield from (k for k in product([1, 0, -1], repeat=dims) if any(k))
+
+    def iter_neighbours(self, loc):
+        for k in self.neighbour_deltas():
+            k = tuple(sum(p) for p in zip(loc, k))
+            yield self.get(k, False)
+
+    def cycle(self, n):
+        for _ in range(n):
+            new = Grid()
+            bounds = product(*[range(a - 1, b + 2) for a, b in self.bounds()])
+            for i in bounds:
+                active = self.get(i, False)
+                count = 0
+                for n in self.iter_neighbours(i):
+                    count += n
+                if (active and count in (2, 3)) or (not active and count == 3):
+                    new[i] = True
+            self.clear()
+            self.update(new)
+
+
 lines = [l.strip() for l in open(sys.argv[1])]
-DELTAS = [1, 0, -1]
+grid = Grid({(j, i, 0): c == "#" for i, l in enumerate(lines) for j, c in enumerate(l)})
+grid.cycle(6)
+print(f"Part 1: {sum(grid.values())}")
 grid = {(j, i, 0, 0): c == "#" for i, l in enumerate(lines) for j, c in enumerate(l)}
-
-
-def get_bounds(g):
-    min_ = list(map(min, zip(*g.keys())))
-    max_ = list(map(max, zip(*g.keys())))
-    return list(zip(min_, max_))
-
-
-def cycle(g):
-    new = {}
-    (x1, x2), (y1, y2), (z1, z2), (w1, w2) = get_bounds(g)
-    for x, y, z, w in product(
-        range(x1 - 1, x2 + 2),
-        range(y1 - 1, y2 + 2),
-        range(z1 - 1, z2 + 2),
-        range(w1 - 1, w2 + 2),
-    ):
-        active = g.get((x, y, z, w), False)
-        count = 0
-        for dx, dy, dz, dw in product(DELTAS, DELTAS, DELTAS, DELTAS):
-            if dx == dy == dz == dw == 0:
-                continue
-            count += g.get((x + dx, y + dy, z + dz, w + dw), False)
-        if (active and count in (2, 3)) or (not active and count == 3):
-            new[(x, y, z, w)] = True
-    return new
-
-
-for _ in range(6):
-    grid = cycle(grid)
+grid = Grid(grid)
+grid.cycle(6)
 print(f"Part 2: {sum(grid.values())}")
