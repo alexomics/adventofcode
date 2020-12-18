bdbbd773226695c7cd46c8f3165039dfc0bd4214
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-18 12:52:49 +0000

    2020 day 18
    Refactor to make `parse` a recursive function.
    
    Other changes:
     - Pass `eval_` function instead of a `part1` flag
     - Use `re.finditer` properly, no more adding parentheses to evaluate
       expressions
     - Use `partial` and `map` for more readable code

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,9 +1,13 @@
 import sys
 import re
+from functools import partial
+
+
+pat = re.compile(r"\(([\d+* ]*?)\)")
 
 
 def lr_eval(s):
-    p = s[1:-1].split()[::-1]
+    p = s.split()[::-1]
     res = int(p.pop())
     while p:
         curr = p.pop()
@@ -15,7 +19,7 @@ def lr_eval(s):
 
 
 def rev_eval(s):
-    p = s[1:-1].split()[::-1]
+    p = s.split()[::-1]
     res = []
     x = int(p.pop())
     while p:
@@ -26,23 +30,17 @@ def rev_eval(s):
             res.extend([x, "*"])
             x = int(p.pop())
     res.append(x)
-    return lr_eval(f"({' '.join(map(str, res))})")
+    return lr_eval(" ".join(map(str, res)))
 
 
-def parse(s, part1=True):
-    pat = re.compile(r"\(([\d+* ]*?)\)")
-    s = f"({s})"
-    while "(" in s:
-        m = pat.finditer(s)
-        for g in m:
-            a, b = g.span()
-            if part1:
-                s = s[:a] + str(lr_eval(g.group())) + s[b:]
-            else:
-                s = s[:a] + str(rev_eval(g.group())) + s[b:]
-            break
-    return int(s)
+def parse(s, eval_):
+    if "(" in s:
+        for m in reversed(list(pat.finditer(s))):
+            s = s[: m.start()] + str(parse(m.group(1), eval_)) + s[m.end() :]
+        return parse(s, eval_)
+    return eval_(s)
 
 
-print(f"Part 1: {sum(parse(l.strip()) for l in open(sys.argv[1]))}")
-print(f"Part 2: {sum(parse(l.strip(), part1=False) for l in open(sys.argv[1]))}")
+lines = open(sys.argv[1]).readlines()
+print(f"Part 1: {sum(map(partial(parse, eval_=lr_eval), lines))}")
+print(f"Part 2: {sum(map(partial(parse, eval_=rev_eval), lines))}")
