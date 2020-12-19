cc17e9bf23ac595f43d72d01709a2939e8b1ded6
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-12-19 08:13:06 +0000

    2020 day 19
    Part 2

--- 2020/day19.py
+++ 2020/day19.py
@@ -12,6 +12,7 @@ messages = messages.splitlines()
 
 @lru_cache
 def make_strs(i):
+    """Make all strings starting from `i`"""
     rule = rules[i]
     if isinstance(rule, str):
         return rule[1]
@@ -23,5 +24,24 @@ def make_strs(i):
 
 
 valid_strs = set(make_strs("0"))
-a = sum(m in valid_strs for m in messages)
-print(f"Part 1: {a}")
+print(f"Part 1: {sum(m in valid_strs for m in messages)}")
+
+
+def make_str_from_rule(s, seq):
+    """Attempt to make string `s` from rule `seq`"""
+    if not s or not seq:
+        return s == "" and seq == []
+    rule = rules[seq[0]]
+    if isinstance(rule, str):
+        # Strip first character, False if cannot
+        return make_str_from_rule(s[1:], seq[1:]) if s[0] in rule else False
+    else:
+        # Expand first term in seq rules list
+        return any(make_str_from_rule(s, t + seq[1:]) for t in rule)
+
+
+# Add rules for part 2
+rules["8"] = [["42"], ["42", "8"]]
+rules["11"] = [["42", "31"], ["42", "11", "31"]]
+
+print(f"Part 2: {sum(make_str_from_rule(m, ['0']) for m in messages)}")
