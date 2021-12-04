93254ed5367e263e6b2a4296827dd2bb649494a5
Alex Payne <alexander.payne@nottingham.ac.uk>
2021-12-04 06:25:08 +0000

    2021 day 4

--- 2021/day04.py
+++ 2021/day04.py
@@ -1,7 +1,6 @@
 import sys
 
 sequence, *boards = sys.stdin.read().split("\n\n")
-
 sequence = [int(x) for x in sequence.split(",")]
 boards = [[[int(x) for x in l.split()] for l in b.strip().split("\n")] for b in boards]
 
@@ -11,25 +10,21 @@ def wins(board, nums):
     return any(all(x in nums for x in rc) for rc in rows_and_cols)
 
 
-winning_board = None
-last_number = None
+winner = False
 seen_nums = set()
 for n in sequence:
     seen_nums.add(n)
 
     for board in boards:
         if wins(board, seen_nums):
-            winning_board = board
-            last_number = n
+            not_seen = sum([i for r in board for i in r if i not in seen_nums])
+            print(f"Part 1: {not_seen * n}")
+            winner = True
             break
-    if winning_board is not None:
+
+    if winner:
         break
 
-ns = [x for r in winning_board for x in r if x not in seen_nums]
-print(f"Part 1: {sum(ns) * last_number}")
-
-winning_board = None
-last_number = None
 seen_nums = set()
 for n in sequence:
     seen_nums.add(n)
@@ -39,7 +34,6 @@ for n in sequence:
             boards.remove(board)
     if not boards:
         # None left
-        not_called = set(x for r in board for x in r)
-        x = sum(not_called - seen_nums)
-        print(f"Part 2: {n * x}")
+        not_called = set(i for r in board for i in r)
+        print(f"Part 2: {n * sum(not_called - seen_nums)}")
         break
