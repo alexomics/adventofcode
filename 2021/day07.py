0d0d3f6310f23eabec94b2ff3c6df33c804cd6c6
Alex Payne <alexander.payne@nottingham.ac.uk>
2021-12-07 06:15:26 +0000

    2021 day 7

---
+++ 2021/day07.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def arithemtic_sum(n):
+    return n * (n + 1) // 2
+
+
+def solve(locs, f=lambda x: x):
+    res = float("inf")
+    for target in range(min(locs), max(locs) + 1):
+        cost = sum(f(abs(crab - target)) for crab in locs)
+        if cost < res:
+            res = cost
+    return res
+
+
+lines = [int(x) for x in sys.stdin.read().split(",")]
+print(f"Part 1: {solve(lines)}")
+print(f"Part 1: {solve(lines, arithemtic_sum)}")
