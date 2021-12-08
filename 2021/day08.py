bb9b15b01067dac0bd3352086c3552f790154434
Alex Payne <alexander.payne@nottingham.ac.uk>
2021-12-08 10:50:11 +0000

    2021 day 8
    Cleaned up parsing. Use `frozenset' as this simplifies the logic a
    little bit. Also add comments to the filter functions.

--- 2021/day08.py
+++ 2021/day08.py
@@ -2,26 +2,38 @@ import sys
 
 
 def get_nine(four, maybe_nine):
+    """four and nine share exactly four segments
+    false for all other 6-segment numbers [0, 6]
+    """
     return len(four & maybe_nine) == 4
 
 
 def get_three(seven, maybe_three):
+    """difference between three and seven is exactly 2
+    other 5-segment numbers [2, 5] have a differnece of 3
+    """
     return len(maybe_three - seven) == 2
 
 
 def get_two(nine, maybe_two):
+    """difference between two and nine is exactly 1
+    other 5-segment numbers [3, 5] entirely overlap
+    """
     return maybe_two - nine
 
 
 def get_five(two, three, maybe_five):
+    """five is the last 5-segment number so remove both two and three"""
     return maybe_five != two and maybe_five != three
 
 
 def get_six(one, eight, maybe_six):
+    """six, eight, and one overlap in exactly 1 segment"""
     return len(maybe_six & eight & one) == 1
 
 
 def get_zero(six, nine, maybe_zero):
+    """zero is the last 6-segment number so remove both six and nine"""
     return maybe_zero != six and maybe_zero != nine
 
 
@@ -29,16 +41,13 @@ seg_nums = {2: [1], 3: [7], 4: [4], 5: [2, 3, 5], 6: [0, 6, 9], 7: [8]}
 
 
 def decode(entry):
-    # Possible values
-    p = {i: [] for i in range(10)}
     pats, code = entry.strip().split("|")
+    # `p' will hold possible values for each digit
+    p = {i: set() for i in range(10)}
     for signal in pats.split() + code.split():
         for s in seg_nums[len(signal)]:
-            p[s].append(set(signal))
-    p = {
-        k: [set(vv) for vv in set(tuple(sorted(v_)) for v_ in v)] for k, v in p.items()
-    }
-    p = {k: v[0] if len(v) == 1 else v for k, v in p.items()}
+            p[s].add(frozenset(signal))
+    p = {k: v.pop() if len(v) == 1 else v for k, v in p.items()}
     p[9] = [s for s in p[9] if get_nine(p[4], s)][0]
     p[3] = [s for s in p[3] if get_three(p[7], s)][0]
     p[2] = [s for s in p[2] if get_two(p[9], s)][0]
