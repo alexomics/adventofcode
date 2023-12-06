8dac4eed661a5338100b7ac9c34cbf6edafb0031
Alexander Payne <alex.payne22@gmail.com>
2023-12-06 05:38:16 +0000

    2023 day 6
    Started late today, brute force works for both parts so if there's a
    nicer solution I'll have to find out about it later...
    
    My initial solution was much more nested/loopy but the reduced solution
    is quite nice thing to look at!

---
+++ 2023/day06.py
@@ -0,0 +1,18 @@
+import sys
+from math import prod
+from itertools import starmap
+
+
+def parse(line):
+    return [int(x) for x in line.split(":")[1].split()]
+
+
+def f(time, dist):
+    return sum(i * (time - i) > dist for i in range(time + 1))
+
+
+time, dist = map(parse, sys.stdin)
+print(prod(starmap(f, zip(time, dist))))
+time = [int("".join(map(str, time)))]
+dist = [int("".join(map(str, dist)))]
+print(prod(starmap(f, zip(time, dist))))
