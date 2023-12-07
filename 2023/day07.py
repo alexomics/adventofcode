8ab28aa9a88c88fa1d68931253a81048b91248ff
Alexander Payne <alex.payne22@gmail.com>
2023-12-07 09:15:35 +0000

    2023 day 7
    Interesting but wordy day. Initial solution came with copious amounts of
    spaghetti, this one is still a right casserole.
    
    ---
    
    Update: Much nicer! actually readable.
    
    I did, at one point, think this would be a functools.cmp_to_key journey.
    I'm very happy it wasn't! Other ideas that weren't fruitful include:
    - Enums (cards and hands)
    - dataclass using ordering (didn't try)

--- 2023/day07.py
+++ 2023/day07.py
@@ -1,93 +1,46 @@
 import sys
-from collections import Counter, deque
-
-
-strength = {k: v for v, k in enumerate(reversed("AKQJT98765432"), start=2)}
+from collections import Counter
 
 
 def parse(stdin):
+    strength = {k: v for v, k in enumerate("23456789TJQKA", start=2)}
     for line in stdin:
         ca, b = line.split()
         yield tuple(strength.get(c) for c in ca), int(b)
 
 
-# Five of a kind, where all five cards have the same label: AAAAA
-def five_of_a_kind(cards):
-    return len(set(cards)) == 1
+fs = dict(
+    high_card=lambda c: c == [1, 1, 1, 1],
+    one_pair=lambda c: c == [1, 1, 1, 2],
+    two_pair=lambda c: c == [1, 2, 2],
+    three_of_a_kind=lambda c: c == [1, 1, 3],
+    full_house=lambda c: c == [2, 3],
+    four_of_a_kind=lambda c: c == [1, 4],
+    five_of_a_kind=lambda c: c == [5],
+)
 
 
-# Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-def four_of_a_kind(cards):
-    c = Counter(cards)
-    return 4 in c.values() and 1 in c.values()
+def score(cards: Counter[int, int]):
+    g = (i for i, f in enumerate(fs.values(), 1) if f(sorted(cards.values())))
+    return max(g, default=0)
 
 
-# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-def full_house(cards):
-    c = Counter(cards)
-    return 3 in c.values() and 2 in c.values()
-
-
-# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-def three_of_a_kind(cards):
-    c = Counter(cards)
-    return 3 in c.values()
-
-
-# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-def two_pair(cards):
-    c = Counter(cards)
-    return list(c.values()).count(2) == 2
-
-
-# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-def one_pair(cards):
-    c = Counter(cards)
-    return list(c.values()).count(2) == 1
-
-
-# High card, where all cards' labels are distinct: 23456
-def high_card(cards):
-    return len(set(cards)) == 5
-
-
-fs = [
-    five_of_a_kind,
-    four_of_a_kind,
-    full_house,
-    three_of_a_kind,
-    two_pair,
-    one_pair,
-    high_card,
-]
-
-res = []
-p2 = []
+p1, p2 = [], []
 for cards, bid in parse(sys.stdin):
-    # print(cards, " >> ", bid)
-    rank = deque([rank for rank, f in enumerate(fs[::-1], 1) if f(cards)], maxlen=1)[0]
-    res.append((rank, cards, bid))
+    p1.append((score(Counter(cards)), cards, bid))
 
-    c = tuple(c if c != 11 else 1 for c in cards)
-    c2 = Counter(c if c != 11 else 1 for c in cards)
-    for k, v in [(k, v) for k, v in c2.most_common(5) if k != 1]:
-        c2[k] += c2.get(1, 0)
-        del c2[1]
+    cards = tuple(c if c != 11 else 1 for c in cards)
+    counts = Counter(cards)
+    for k, v in counts.most_common(5):
+        if k == 1:
+            continue
+        counts[k] += counts.get(1, 0)
+        del counts[1]
         break
-    c2_ = []
-    for k, v in c2.items():
-        c2_.extend([k] * v)
-    c2_ = tuple(c2_)
-    rank = deque([rank for rank, f in enumerate(fs[::-1], 1) if f(c2_)], maxlen=1)[0]
-    p2.append((rank, c, bid))
+    cards_ = tuple(k for k, v in counts.items() for _ in range(v))
+    p2.append((score(Counter(cards_)), cards, bid))
 
-res.sort()
-x = 0
-for i, (_, _, b) in enumerate(res, 1):
-    x += (i) * b
-print(x)
+p1.sort()
 p2.sort()
-x = 0
-for i, (_, _, b) in enumerate(p2, 1):
-    x += (i) * b
-print(x)
+print(sum(i * b for i, (_, _, b) in enumerate(p1, 1)))
+print(sum(i * b for i, (_, _, b) in enumerate(p2, 1)))
