420f980c2b738f5198f825c71197d1e592eea7d4
Alexander Payne <alex.payne22@gmail.com>
2024-12-20 06:12:22 +0000

    2024 day 20

--- 2024/day20.py
+++ 2024/day20.py
@@ -1,6 +1,5 @@
 import sys
-import heapq
-from collections import defaultdict, deque, Counter
+from collections import deque
 
 NORTH = (0, -1)
 EAST = (1, 0)
@@ -11,31 +10,42 @@ data = sys.stdin.read().strip().splitlines()
 grid = {(x, y): c for y, l in enumerate(data) for x, c in enumerate(l)}
 start = next(k for k, v in grid.items() if v == "S")
 end = next(k for k, v in grid.items() if v == "E")
-mx = max(x for x, _ in grid)
-my = max(y for _, y in grid)
 
 
-def solve(grid, pos, end, mx, my):
-    todo = deque([(*pos, 0)])
+def manhattan_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def solve(grid, pos, end):
+    todo = deque([pos])
     seen = set()
+    path = []
     while todo:
-        x, y, s = todo.popleft()
-        if (x, y) == end:
-            return s
+        x, y = todo.popleft()
         if (x, y) in seen:
             continue
+        path.append((x, y))
         seen.add((x, y))
         for dx, dy in DIRS:
             x_ = x + dx
             y_ = y + dy
-            if 0 <= x_ < mx and 0 <= y_ < mx and grid.get((x_, y_), ".") != "#":
-                todo.append((x_, y_, s + 1))
+            if grid.get((x_, y_), "") in ".SE":
+                todo.append((x_, y_))
+        if (x, y) == end:
+            return path
 
 
-path = solve(grid, start, end, mx, my)
-c = Counter(
-    solve(grid | {pos: "."}, start, end, mx, my)
-    for pos, char in grid.items()
-    if char == "#"
-)
-print(sum(v for k, v in c.items() if path - k >= 100))
+def cheat(path, steps, saving=100):
+    shortcuts = set()
+    for i, p1 in enumerate(path, 0):
+        for j, p2 in enumerate(path[i + 1 :], i + 1):
+            if (m := manhattan_distance(p1, p2)) <= steps:
+                if j - i - m >= saving:
+                    shortcuts.add((p1, p2, m))
+    return shortcuts
+
+
+path = solve(grid, start, end)
+pp1 = cheat(path, 2, 100)
+pp2 = cheat(path, 20, 100)
+print(len(pp1), len(pp2), sep="\n")
