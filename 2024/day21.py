4b54f14c414012b8113990ac1e3db782c1d6b583
Alexander Payne <alex.payne22@gmail.com>
2024-12-21 07:38:57 +0000

    2024 day 21
    part 1 need caches or counters for part 2

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,128 +1,56 @@
 import sys
-from collections import deque
+from collections import deque, Counter
 from functools import cache
 
-
-def pairwise(iterable):
-    # pairwise('ABCDEFG') → AB BC CD DE EF FG
-
-    iterator = iter(iterable)
-    a = next(iterator, None)
-
-    for b in iterator:
-        yield a, b
-        a = b
-
-
-"""
-code='029A' (4)
-      029A
-code='<A^A^^>AvvvA' (12)
-      <A^A>^^AvvvA
-code='v<<A>>^A<A>AvA^<AA>Av<AAA>^A' (28)
-      v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-
-      <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-
-"""
-DIRS_MAP = {
-    "<": (-1, 0),
-    "v": (0, 1),
-    ">": (1, 0),
-    "^": (0, -1),
-}
-DIRS = {v: k for k, v in DIRS_MAP.items()}
 # fmt: off
+moves = {">": (1, 0), "v": (0, 1), "<": (-1, 0), "^": (0, -1)}
 numeric = {
     (0, 0): "7", (1, 0): "8", (2, 0): "9",
     (0, 1): "4", (1, 1): "5", (2, 1): "6",
     (0, 2): "1", (1, 2): "2", (2, 2): "3",
                  (1, 3): "0", (2, 3): "A",
 }
+numeric.update({v: k for k, v in numeric.items()})
 directional = {
                  (1, 0): "^", (2, 0): "A",
     (0, 1): "<", (1, 1): "v", (2, 1): ">",
 }
+directional.update({v: k for k, v in directional.items()})
 # fmt: on
 codes = sys.stdin.read().strip().splitlines()
 
-numeric_jumps = {}
-for curr, target, move in [
-    ("7", "8", ">"),
-    ("7", "4", "v"),
-    ("8", "9", ">"),
-    ("8", "5", "v"),
-    ("9", "6", "v"),
-    ("4", "5", ">"),
-    ("4", "1", "v"),
-    ("5", "6", ">"),
-    ("5", "2", "v"),
-    ("6", "3", "v"),
-    ("1", "2", ">"),
-    ("2", "3", ">"),
-    ("2", "0", "v"),
-    ("3", "A", "v"),
-    ("0", "A", ">"),
-]:
-    numeric_jumps[(curr, target)] = move
-    numeric_jumps[(target, curr)] = {"v": "^", ">": "<"}[move]
+
+@cache
+def jump(grid_, curr, target):
+    grid = {0: numeric, 1: directional}[grid_]
+    cx, cy = curr
+    tx, ty = target
+    dx, dy = tx - cx, ty - cy
+    h = "<" * -dx if dx < 0 else ">" * dx
+    v = "^" * -dy if dy < 0 else "v" * dy
+    # Check corners so we don't fall of the grid
+    if dx > 0 and (cx, ty) in grid:
+        return f"{v}{h}A"
+    if (tx, cy) in grid:
+        return f"{h}{v}A"
+    if (cx, ty) in grid:
+        return f"{v}{h}A"
+    raise TabError(f"{curr=} {target=} {(dx,dy)=} {grid=}")
 
 
-# @cache
-# def jump_n(curr, target):
-#     return [(curr, target)]
-
-
-def walk(grid, pos, target):
-    todo = deque([pos])
-    came_from = {pos: None}
-    x, y = pos
-    while todo:
-        x, y = todo.popleft()
-        if grid[(x, y)] == target:
-            break
-        for dx, dy in DIRS:
-            x_, y_ = x + dx, y + dy
-            if (x_, y_) not in came_from and (x_, y_) in grid:
-                todo.append((x_, y_))
-                came_from[(x_, y_)] = (x, y)
-    curr = (x, y)
-    path = []
-    while curr != pos:
-        path.append(curr)
-        curr = came_from[curr]
-    path.append(pos)
-    return path[::-1]
-
-
-s = 0
+p1, p2 = 0, 0
 for code in codes:
     n = int(code[:-1])
-    typed = []
-    positions = deque(
-        [
-            (numeric, 2, 3),
-            (directional, 2, 0),
-            (directional, 2, 0),
-            # (directional, 2, 0),
-        ]
+    pads = deque(
+        [(0, numeric, (2, 3)), (1, directional, (2, 0)), (1, directional, (2, 0))]
     )
-    while positions:
-        k, x, y = positions.popleft()
-        print(f"{code=} ({len(code)})")
+    while pads:
+        g_, g, pos = pads.popleft()
         res = []
         for char in code:
-            path = walk(k, (x, y), char)
-            diff = [(p2[0] - p1[0], p2[1] - p1[1]) for p1, p2 in pairwise(path)]
-            c = "".join([DIRS[d] for d in diff]) + "A"
-            (x, y) = path[-1]
-            print(f"{char=} {c} {k[(x, y)]}")
-            res.append(c)
-        typed.append(res)
+            res.append(jump(g_, pos, g[char]))
+            pos = g[char]
         code = "".join(res)
-    print(f"{code=} ({len(code)})")
+    p1 += len(code) * n
 
-    # print(typed)
-    print(len("".join(typed[-1])), "*", n)
-    s += len("".join(typed[-1])) * n
-print(s)
+print(p1, p2, sep="\n")
