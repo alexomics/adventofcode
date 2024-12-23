8d6775d36e6ed3c7b359532d5e756db1b2e7f1f3
Alexander Payne <alex.payne22@gmail.com>
2024-12-23 07:06:22 +0000

    2024 day 23
    Took ages to understand part 1, caved and looked up part 2. No chance of
    working that one out myself...

--- 2024/day23.py
+++ 2024/day23.py
@@ -1,13 +1,12 @@
 import sys
-from collections import defaultdict, Counter
+from collections import defaultdict
+
 
 data = [l.split("-") for l in sys.stdin.read().strip().splitlines()]
 d = defaultdict(set)
 for a, b in data:
     d[a].add(b)
-    # d[a].add((b, a))
     d[b].add(a)
-    # d[b].add((a, b))
 
 p = set()
 for a in d:
@@ -16,4 +15,13 @@ for a in d:
             if a in d[c]:
                 p.add(tuple(sorted([a, b, c])))
 p1 = sum(any(c[0] == "t" for c in t) for t in p)
+
 print(p1)
+# TY solution megathread for p2
+import networkx as nx
+
+G = nx.Graph(data)
+cliques = nx.find_cliques(G)
+p2 = ",".join(sorted(max(cliques, key=len)))
+
+print(p2)
